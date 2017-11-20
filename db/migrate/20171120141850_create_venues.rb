2c6101e3b5904ed327a2e3927879252dfbf955e6
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-11-20 15:39:08 +0100

    Migration done

--- db/migrate/20171120141850_create_venues.rb
+++ db/migrate/20171120141850_create_venues.rb
@@ -4,7 +4,7 @@ class CreateVenues < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :address
       t.text :description
-      t.date :opening-hours
+      t.date :opening_hours
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120130236) do
+ActiveRecord::Schema.define(version: 20171120143416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_bookings_on_event_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "category"
+    t.string   "tags"
+    t.integer  "price"
+    t.date     "start_time"
+    t.date     "duration"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.date     "date"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,4 +57,17 @@ ActiveRecord::Schema.define(version: 20171120130236) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "venues", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.text     "description"
+    t.date     "opening_hours"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "phone"
+    t.string   "email"
+  end
+
+  add_foreign_key "bookings", "events"
+  add_foreign_key "bookings", "users"
 end
