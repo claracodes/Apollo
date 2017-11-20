3394db1ef3be776c1c48466b747c9cef5ea93722
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-11-20 18:06:10 +0100

    Events controller created and corresponding routes as well. Display of the suggestions on the homepage improved

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'events/show'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlx
+  resources :events, only: [:show]
+
 end
