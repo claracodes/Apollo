00d23ce7be8da11e221384e1be113f27bc6efb98
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-11-21 18:16:32 +0100

    Changes in the registration form - user model validations changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
-
-  validates :first_name, presence: true
-  validates :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
