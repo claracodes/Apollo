538cb2bea4adee17e9c81d8fa12081d53965f8eb
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-11-21 11:57:52 +0100

    Changed the seedfile and delted the validation for host in the user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,14 +16,12 @@ Booking.destroy_all
 puts 'Creating 15 new venues'
 15.times do |venue|
   venue = Venue.new(name: Faker::Ancient.god, description: Faker::Hipster.sentence(10), phone: Faker::PhoneNumber.phone_number, email: Faker::Internet.email)
-  puts venue.inspect
   venue.save!
 end
 
 puts 'Creating 40 new events'
 40.times do |event|
   event = Event.new(name: Faker::Hipster.word, description: Faker::Lorem.sentences(5), category: Event::CATEGORY.sample, date: Faker::Date.forward(300), price: Faker::Commerce.price, venue_id: Venue.all.sample)
-  puts event.inspect
   event.save!
 end
 
@@ -32,8 +30,7 @@ puts 'Creating 25 new users'
   user = User.new(first_name: Faker::Name.first_name,
                   last_name: Faker::Name.last_name,
                   email: Faker::Internet.email,
-                  password: "password",
-                  host: false)
+                  password: "password")
   user.save!
 end
 
