c70f9fb3af01f5b3e62c82d0e61ede617b6e562a
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-11-21 11:13:17 +0100

    Routes added for events and bookings which are nested. Notice on change in the landing page, i changed the file path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'events/show'
-
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlx
-  resources :events, only: [:show]
-  get '/search', to: 'events#search'
-
+  resources :events, only: [:show, :index, :new, :edit] do
+    resources :bookings, only: [:new, :create, :show]
+    collection do                       # collection => no restaurant id in URL
+      get '/search', to: "events#search"  # RestaurantsController#top
+    end
+  end
 end
