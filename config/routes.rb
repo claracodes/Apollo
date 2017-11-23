3e5aab5c3eab51cb11995704be3531c8a486b54f
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-11-23 16:19:49 +0100

    Change for payment page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
   get "bookings/:id", to: 'bookings#show', as: :booking
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlx
   resources :events, only: [:show, :index, :new, :edit, :destroy] do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create] do
+      member do
+        get '/payment', to: "bookings#payment", as: :payment
+      end
+    end
     collection do                       # collection => no restaurant id in URL
       get '/search', to: "events#search"  # RestaurantsController#top
     end
