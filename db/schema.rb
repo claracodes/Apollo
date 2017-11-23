89b811168669b3acd4b65515886df59820b9ed5c
Julien Schmit <julien.schmit@ehl.ch>
2017-11-23 11:29:44 +0100

    added FB login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122131902) do
+ActiveRecord::Schema.define(version: 20171123095502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,11 @@ ActiveRecord::Schema.define(version: 20171122131902) do
     t.string   "first_name"
     t.string   "last_name"
     t.boolean  "host",                   default: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
