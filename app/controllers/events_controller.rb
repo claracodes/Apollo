d13ba512af5a0ee726007cc97549cdd32a2fbcf7
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-11-23 11:13:41 +0100

    Final changes to maps

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,38 +2,19 @@ class EventsController < ApplicationController
 
  before_action :host?, only: [:hostdashboard, :edit, :delete, :create]
 
- def show
-  @event = Event.find(params[:id])
-
-  @venues = Venue.find(@event.venue_id)
-    @hash = Gmaps4rails.build_markers(@venues) do |venue, marker|
-      marker.lat venue.latitude
-      marker.lng venue.longitude
-      marker.infowindow render_to_string(partial: "/venues/map_box", locals: { venue: venue })
-    end
- end
+  def show
+    @event = Event.find(params[:id])
+    @venues = Venue.find(@event.venue_id)
+    for_maps
+  end
 
   def search
     @events = Event.where(city: event_params[:city], category: event_params[:category] )
-    # date: event_params[:date]
-    # third search condition - but we have to have real good seeds for that! :)
-
+        # date: event_params[:date]
+        # third search condition - but we have to have real good seeds for that! :)
     @venues = Venue.where.not(latitude: nil, longitude: nil)
-    @hash = Gmaps4rails.build_markers(@venues) do |venue, marker|
-      marker.lat venue.latitude
-      marker.lng venue.longitude
-      marker.infowindow render_to_string(partial: "/venues/map_box", locals: { venue: venue })
-    end
+    for_maps
   end
-
-  # def search
-  #   params[:search] ||= {}
-  #   @events = Event.all
-  #   # or params[:search].each.... don't forget to change form to simple_form_for :search
-  #   [:category, :city, :date].each do |query|
-  #     @events = @events.where(query => params[:search][query].downcase) unless params[:search][query]
-  #   end
-  # end
 
   def hostdashboard
     @venues = current_user.venues
@@ -45,11 +26,15 @@ class EventsController < ApplicationController
     current_user.host
   end
 
-  private
-
   def event_params
     params.require(:event).permit(:date, :city, :category)
   end
 
-
+  def for_maps
+    @hash = Gmaps4rails.build_markers(@venues) do |venue, marker|
+      marker.lat venue.latitude
+      marker.lng venue.longitude
+      marker.infowindow render_to_string(partial: "/venues/map_box", locals: { venue: venue })
+    end
+  end
 end
