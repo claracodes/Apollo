e84b85c1eae367fd625fc6e2cb9ef0c132755410
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-11-29 08:57:24 +0100

    Calendar styling

--- app/assets/javascripts/init_calendar.js
+++ app/assets/javascripts/init_calendar.js
@@ -4,7 +4,18 @@ $(document).ready(function() {
 
     $('#calendar').fullCalendar({
         // put your options and callbacks here
-          events: '/calendar.json'
-    })
+        fixedWeekCount: false, //Determines the number of weeks displayed in a month view - if true (default), always six
+        eventSources: [
 
+        // your event source
+        {
+            url: '/calendar.json', // the view/events/index.json.jbuilder-file
+            color: '#313030',    // the light-gray from our colours - somehow the variable didn't work here
+            textColor: 'white'
+        }
+        // any other sources...
+     ]
+
+
+    })
 });
