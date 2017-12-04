1c87c38df2c49a294331551a55b388a702bdb448
Julien Schmit <julien.schmit@ehl.ch>
2017-12-04 18:02:07 +0100

    ajax is working for the index, still need to make it work for the price slider and the city

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,25 +1,23 @@
 // Ajax for mood (version 1)
 
-const mood = document.getElementById("search_mood");
-console.log(mood)
+// const mood = document.getElementById("search_mood");
+// console.log(mood)
 
-mood.addEventListener("change", function(e) {
-  const form = document.getElementById("submitSearch");
-  form.click();
-});
+// mood.addEventListener("change", function(e) {
+//   const form = document.getElementById("submitSearch");
+//   form.click();
+// });
 
 // Ajax for all inputs (version 2)
-const filters = document.getElementAll(".search-ajax");
+const filters = document.querySelectorAll(".search-ajax");
+console.log(".search-ajax")
+
+filters.forEach(function(element) {
+  addEventListener("change", function(e) {
+    const form = document.getElementById("submitSearch");
+    form.click();
+  });
+});
 
 
 
-
-
-
-
-
-
-
-
-//getSelectorAll
-// html_input => search-ajax
