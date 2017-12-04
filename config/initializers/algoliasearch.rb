b52e9741f14abffaebeccda07683ef8dcb39fc10
Julien Schmit <julien.schmit@ehl.ch>
2017-12-04 14:18:44 +0100

    saving changes

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,10 +1,10 @@
 AlgoliaSearch.configuration = {
-  application_id: 'YourApplicationID',
-  api_key: 'YourAPIKey',
+  application_id: '8J893PQF95',
+  api_key: 'edd2694d290769f169263fd59ce67e02',
   # Additional configuration
-  # connect_timeout: 2,
-  # receive_timeout: 30,
-  # send_timeout: 30,
-  # batch_timeout: 120,
-  # search_timeout: 5
+  connect_timeout: 2,
+  receive_timeout: 30,
+  send_timeout: 30,
+  batch_timeout: 120,
+  search_timeout: 5
 }
