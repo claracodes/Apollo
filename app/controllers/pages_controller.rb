910b5afb464e07aa69035e7805c087ed48a9f637
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-12-06 10:37:10 +0100

    Design even better

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,7 @@ class PagesController < ApplicationController
 
     if user_signed_in?
       @activities = policy_scope(PublicActivity::Activity).order('created_at desc').where(trackable_type: "Booking").where(owner_id: current_user.friends_ids, owner_type:"User") #
+
       @liked_venues = []
       current_user.find_voted_items.each do |item|
         @liked_venues << item if item.class == Venue
