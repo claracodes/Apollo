41614789de6837b1428ebcbc56a7b261a63d8156
Clara <clara_morgeneyer@web.de>
2017-12-06 09:09:35 +0100

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,7 @@
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
 //= require selectize
-
+//= require underscore
 //= require gmaps/google
 //= require moment
 //= require fullcalendar
