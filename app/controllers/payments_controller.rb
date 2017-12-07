9b3651714cf3ead9f7c65bc619fb3a3d9fe3dff0
Clara Morgeneyer <clara_morgeneyer@web.de>
2017-12-07 11:47:58 +0100

    fixed free booking and the big error

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,13 +2,25 @@ class PaymentsController < ApplicationController
   before_action :set_booking
 
   def new
-    #...
+
     @event = Event.find(params[:event_id])
     @booking = Booking.find(params[:booking_id])
   end
 
   def create
-    # ...
+    customer = Stripe::Customer.create(
+      source: params[:stripeToken],
+      email:  params[:stripeEmail]
+      )
+
+    charge = Stripe::Charge.create(
+    customer:     customer.id,   # You should store this customer id and re-use it.
+    amount:       @booking.amount_cents,
+    description:  "Payment for #{@booking.id}",
+    currency:     @booking.amount.currency
+    )
+
+    @booking.update(payment: charge.to_json, state: 'paid')
     redirect_to @booking
   end
 
