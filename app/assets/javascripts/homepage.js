9512f229c8dbb0d1564083498f9e47a442fbea4c
vnnnlc <vnnnlc@gmail.com>
2017-12-08 10:55:55 +0100

    Brand new homepage

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -5,21 +5,21 @@ $(".be-inspired").click(function() {
 });
 
 
-$(".list-moods .mood").click(function(e) {
-  e.preventDefault();
-  // fetch value of button (nerdy, ...)
-  var mood = $(this).data("mood") // returns the value data-mood of the li
-  // change value of #hidden-mood
-  $('#hidden-mood').val(mood)
-  // submit the value to the whole simple_form
-  $('#home-form').submit();
-});
+// $(".list-moods .mood").click(function(e) {
+//   e.preventDefault();
+//   // fetch value of button (nerdy, ...)
+//   var mood = $(this).data("mood") // returns the value data-mood of the li
+//   // change value of #hidden-mood
+//   $('#hidden-mood').val(mood)
+//   // submit the value to the whole simple_form
+//   $('#home-form').submit();
+// });
 
-$(".normal-search").click(function(e) {
-  e.preventDefault();
-  // fetch value of button (nerdy, ...)
-  var search = $(this).data("search") // returns the value data-mood of the li
-  // submit the value to the whole simple_form
-  $('#home-form').submit();
-});
+// $(".normal-search").click(function(e) {
+//   e.preventDefault();
+//   // fetch value of button (nerdy, ...)
+//   var search = $(this).data("search") // returns the value data-mood of the li
+//   // submit the value to the whole simple_form
+//   $('#home-form').submit();
+// });
 
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -29,7 +29,7 @@ $(function () {
   var numOfOptions = $('#cusSelectbox1').children('option').length;
 
   // hide select tag
-  defaultselectbox.addClass('s-hidden');
+  defaultselectbox.addClass('s-hidden1');
 
   // wrapping default selectbox into custom select block
   defaultselectbox.wrap('<div class="cusSelBlock1"></div>');
@@ -122,7 +122,7 @@ $(function () {
   var numOfOptions = $('#cusSelectbox2').children('option').length;
 
   // hide select tag
-  defaultselectbox.addClass('s-hidden');
+  defaultselectbox.addClass('s-hidden2');
 
   // wrapping default selectbox into custom select block
   defaultselectbox.wrap('<div class="cusSelBlock2"></div>');
@@ -192,6 +192,103 @@ $(function () {
 function focusItems() {
 
   $('.options2').on('focus', 'li', function() {
+    $this = $(this);
+    $this.addClass('active').siblings().removeClass();
+  }).on('keydown', 'li', function(e) {
+    $this = $(this);
+    if (e.keyCode == 40) {
+      $this.next().focus();
+      return false;
+    } else if (e.keyCode == 38) {
+      $this.prev().focus();
+      return false;
+    }
+  }).find('li').first().focus();
+
+}
+
+
+
+
+
+// DROPDOWN FILTER BAR //
+
+$(function () {
+
+  var defaultselectbox = $('#cusSelectbox3');
+  var numOfOptions = $('#cusSelectbox3').children('option').length;
+
+  // hide select tag
+  defaultselectbox.addClass('s-hidden3');
+
+  // wrapping default selectbox into custom select block
+  defaultselectbox.wrap('<div class="cusSelBlock3"></div>');
+
+  // creating custom select div
+  defaultselectbox.after('<div class="selectLabel3"></div>');
+
+  // getting default select box selected value
+  $('.selectLabel3').text(defaultselectbox.children('option').eq(0).text());
+
+  // appending options to custom un-ordered list tag
+  var cusList = $('<ul/>', { 'class': 'options3 select required'} ).insertAfter($('.selectLabel3'));
+
+  // generating custom list items
+  for(var i=0; i< numOfOptions; i++) {
+    $('<li/>', {
+    text: defaultselectbox.children('option').eq(i).text(),
+    rel: defaultselectbox.children('option').eq(i).val()
+    }).appendTo(cusList);
+  }
+
+  // open-list and close-list items functions
+  function openList() {
+    for(var i=0; i< numOfOptions; i++) {
+      $('.options3').children('li').eq(i).attr('tabindex', i).css(
+        'transform', 'translateY('+(i*100+100)+'%)').css(
+        'transition-delay', i*30+'ms');
+    }
+  }
+
+  function closeList() {
+    for(var i=0; i< numOfOptions; i++) {
+      $('.options3').children('li').eq(i).css(
+        'transform', 'translateY('+i*0+'px)').css('transition-delay', i*0+'ms');
+    }
+    // $('.options').children('li').eq(1).css('transform', 'translateY('+2+'px)');
+    // $('.options').children('li').eq(2).css('transform', 'translateY('+4+'px)');
+  }
+
+  // click event functions
+  $('.selectLabel3').click(function () {
+    $(this).toggleClass('active');
+    if( $(this).hasClass('active') ) {
+      openList();
+      focusItems();
+    }
+    else {
+      closeList();
+    }
+  });
+
+  $(".options3 li").on('keypress click', function(e) {
+    e.preventDefault();
+    $('.options3 li').siblings().removeClass();
+    closeList();
+    $('.selectLabel3').removeClass('active');
+    $('.selectLabel3').text($(this).text());
+    defaultselectbox.val($(this).text());
+
+    var form = document.getElementById("submitSearch");
+    form.click();
+    // $('.selected-item p span').text($('.selectLabel').text());
+  });
+
+});
+
+function focusItems() {
+
+  $('.options3').on('focus', 'li', function() {
     $this = $(this);
     $this.addClass('active').siblings().removeClass();
   }).on('keydown', 'li', function(e) {
